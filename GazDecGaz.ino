0d91ae6f076140482588569f81b750c48bf88346
javiernetes <xavi@santmodest.net>
2023-11-09 13:35:57 +0100

    Update GazDecGaz.ino
    acabado Eqqiez 280 má sligero que adafruit a l aespera si ens160 cabe o sustiruir por mas ligera.

--- GazDecGaz.ino
+++ GazDecGaz.ino
@@ -6,15 +6,12 @@
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
-//#include <Wire.h>
+#include <Wire.h>
 //#include <SPI.h>
-#include <Adafruit_BME280.h>
+#include <ErriezBMX280.h>
 #include <ScioSense_ENS160.h>
+ErriezBMX280 bmx280 = ErriezBMX280(0x76);
 
-Adafruit_BME280 bme; // use I2C interface
-Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
-Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
-Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
 const int butPIN = 8;
 const int ledPIN = 9;
 const int buzPIN = 11;
@@ -27,16 +24,14 @@ const int PIN_SCLK = 7;   // LCD5 Clk
               // LCD7 Vled
               // LCD8 Gnd
 byte estaon = HIGH;
-bool emergency = 0; // algo no va bien
-bool alert = 0; // bocinazo + flash
-bool ack = 0; // piloto ha apretado el botos de aceptado
-bool ackhPa = 0; // piloto ha apretado el botos de aceptado en hPa alarm
-bool ackCo2 = 0; // piloto ha apretado el botos de aceptado en Co2alarm
+bool alert = 0; // bocinazo + flash comtrol de tiempo millis
+bool ackhPa = 0; // piloto ha apretado el boton de aceptado en hPa alarm
+bool ackCo2 = 0; // piloto ha apretado el boton de aceptado en Co2alarm
 bool emerTemp = 0; // se ha activado emergencia por Temteratura
 bool emerhPa= 0; // se ha activado emergencia por altura -> O2
 bool emerAQI= 0; // alerta > 3 activa Air quality index alert
 bool emerTVOC= 0; //Total volatile organic compounds > 750
-bool emereCO2= 0; // > 1000 activa alerta
+bool emerCo2= 0; // > 1000 activa alerta
 int maxhPa = 1044; //max presión registrada
 int minhPa = 700; //presion a 3000 metros HIPOXIA
 int curTemp = 0; //temperatura actual
@@ -55,7 +50,7 @@ void setup() {
   pinMode(buzPIN, OUTPUT); // pin Buz en salida
   pinMode(butPIN, INPUT); // pin boton es entrada
   display.begin();
-  // init done
+  // init DISPLAY NOKIA 5110
   display.setContrast(50);
   display.setRotation(0);
   display.clearDisplay();   // clears the screen and buffer
@@ -77,25 +72,13 @@ void setup() {
   delay (500);
   noTone(buzPIN);
   delay (10000); //10 segundos de screenshot credits 
-  digitalWrite(ledPIN, LOW); // led ON
-  
-  Serial.println(F("BME280 Sensor event test"));
-  if (!bme.begin()) {
-    Serial.println(F("No se ha encontrado el sensor BME280"));
-    while (1) delay(10);
+  digitalWrite(ledPIN, LOW); // led ON 10 segundos
+  // init BMP280
+  while (!bmx280.begin()) {
+        Serial.println(F("Error: Could not detect sensor"));
+        delay(1000);
   }
-  bme_temp->printSensorDetails();
-  bme_pressure->printSensorDetails();
-  bme_humidity->printSensorDetails();
-
-  Serial.println("------------------------------------------------------------");
-  Serial.println("ENS160 - Digital air quality sensor");
-  Serial.println();
-  Serial.println("Sensor readout in standard mode");
-  Serial.println();
-  Serial.println("------------------------------------------------------------");
-  delay(1000);
-
+  //init ens160
   Serial.print("ENS160...");
   ens160.begin();
   Serial.println(ens160.available() ? "done." : "failed!");
@@ -108,13 +91,17 @@ void setup() {
     Serial.println(ens160.setMode(ENS160_OPMODE_STD) ? "done." : "failed!");
   }
 }//setup
-
+//---------------------------------------------------------
 
 void loop() {
   leebme280();
   leeens160();
-  flash();
   ackButton();
+  if ((emerhPa==1)||(emerCo2==1)){ //si hay emergencia 
+     if ((ackhPa==0)&&(ackCo2==0){ //si no hay ack's del Piloto
+       flash(); //pita y flash
+     }
+  }
   muestra();
 }
 
@@ -122,7 +109,7 @@ void  flash() {
     //{period}: Periodo de Tiempo en el cual se va a ejecutar esta tarea
     unsigned long period=500; //En Milisegundos
     static unsigned long previousMillis=0;
-    if(((millis()-previousMillis)>period)&&(ack=0)){//si priodo y el piloto no ha dicho que basta
+    if(((millis()-previousMillis)>period){//si priodo y el piloto no ha dicho que basta
       if (alert=0){
         digitalWrite(ledPIN, HIGH); // led ON
         tone(buzPIN, 880, 200);
@@ -131,6 +118,7 @@ void  flash() {
       else {
         noTone(buzPIN);
         digitalWrite(ledPIN, LOW); // led Off
+        alert=0;
       }
         previousMillis += period;
     }  //millis
@@ -140,36 +128,76 @@ void leebme280(){
     unsigned long period=5000; //En Milisegundos
     static unsigned long previousMillis=0;
     if((millis()-previousMillis)>period){//leer cada 5 segundos
-      sensors_event_t temp_event, pressure_event, humidity_event;
-      bme_temp->getEvent(&temp_event);
-      bme_pressure->getEvent(&pressure_event);
-      bme_humidity->getEvent(&humidity_event);
-      Serial.print(F("Temperatura = "));
-      Serial.print(temp_event.temperature);
-      Serial.println(" *C");
-      Serial.print(F("Humedad = "));
-      Serial.print(humidity_event.relative_humidity);
-      Serial.println(" %");
-      Serial.print(F("Presion = "));
-      Serial.print(pressure_event.pressure);
-      Serial.println(" hPa");
-      Serial.println();
+      Serial.print(F("Temperature: "));
+      Serial.print(bmx280.readTemperature());
+      Serial.println(" C");
+      curTemp=bmx280.readTemperature();
+      if (bmx280.getChipID() == CHIP_ID_BME280) {
+        Serial.print(F("Humidity:    "));
+        Serial.print(bmx280.readHumidity());
+        Serial.println(" %");
+        curHumi=bmx280.readHumidity();
+        }
+      Serial.print(F("Pressure:    "));
+      Serial.print(bmx280.readPressure() / 100.0F);
+      Serial.println(" hPa")
+      curhPa=bmx280.readPressure() / 100.0F;
       previousMillis += period;
       // test emergencia
-      if (temp_event.temperature < -5 || temp_event.temperature > 40){
-        emerTemp = 1; // se ha activado emergencia por Temteratura
+      if (curTemp < -5 && curTemp > 40){
+        emerTemp = 1; // se ha activado emergencia por Temteratura inf -5 o sup 40 Cº
       }
       else {
         emerTemp = 0;
       }
-      if (pressure_event.pressure < minhPa || pressure_event.pressure > maxhPa){
+      if (curhPa < minhPa && curhPa > maxhPa){
         emerhPa = 1; // se ha activado emergencia por presión
       }
       else {
         emerhPa = 0;
+        ackhPa =0;
       }
     } //millis
 }// finlee 280
+//----------------------------------
+void leeens160(){
+  unsigned long period=7000; //En Milisegundos
+  static unsigned long previousMillis=0;
+  if((millis()-previousMillis)>period){//leer cada 7 segundos
+   if (ens160.available()) {
+    ens160.measure(true);
+    ens160.measureRaw(true);
+    Serial.print("AQI: ");Serial.print(ens160.getAQI());Serial.print("\t");
+    Serial.print("emerTVOC: ");Serial.print(ens160.getTVOC());Serial.print("ppb\t");
+    Serial.print("eCO2: ");Serial.print(ens160.geteCO2());Serial.print("ppm\t");
+    curCo2= ens160.getAQI();
+    curQAI= ens160.getTVOC(); 
+    curTVOC= ens160.geteCO2();
+    }
+    // EValuar emergencia 
+    //bool emerAQI= 0; // alerta >= 3 activa Air quality index alert
+    //bool emerTVOC= 0; //Total volatile organic compounds > 750
+    //bool emerCo2= 0; // > 1000 activa alerta  
+    if (curCo2 > 1000) {
+      emerCo2=1;
+    else{
+      emerCo2=0;
+      ackC02=0;
+      }
+    if (curQAI > 2) {
+      emerQAI=1;
+    else{
+      emerQAI=0;
+      }
+    if (curTVOC > 750) {
+      emerTVOC=1;
+    else{
+      emerTVOC=0;
+      }      
+    previousMillis += period;
+  }//millis
+}// leeens160
+//---------------------------------------------
 
 void ackButton(){  
   estaon = digitalRead(butPIN);
@@ -177,8 +205,19 @@ void ackButton(){
     // ESPERAMOS ANTES DE COMPROBAR NUEVAMENTE
    delay(50);
    if (estaon == LOW ) {
-      ack=1;
-    }
+     if (emerhPa==1)  {
+     ackhPa=1;
+     }
+     else  {
+       ackhPa=0;
+       if (emerCo2==1){
+        ackC02=1;  
+       }
+       else {
+         ackCo2=0;
+       }
+     }
+   }
   }
 }// ackButton
 
@@ -238,35 +277,32 @@ void muestra(){
          display.setTextColor(BLACK);
        }
        display.print("Temp: ");
-       display.println(curTemp);
+       display.print(curTemp);
        display.println(" Cº");
      //-----------------
        display.print("Hume: ");
        display.print(curHume);
        display.println(" %");
      //-----------------
-       display.println("AQi: 1 EXCELL");
-     //-----------------
-       display.println("TVOC: 1 EXCELL");
+       if (emerAQI==1){
+         display.setTextColor(WHITE, BLACK);}
+       else {
+         display.setTextColor(BLACK);
+       }
+       display.print("AirQI: ");
+       display.println(curAQI);
+    //-----------------
+       if (emerAQI==1){
+         display.setTextColor(WHITE, BLACK);}
+       else {
+         display.setTextColor(BLACK);
+       }
+       display.print("TVOC: ");
+       display.println(emerTVOC);
+     
        display.display();
    } 
-  }
-  
-    
+  } 
 
 }//fin muestra
 
-void leeens160(){
-  unsigned long period=7000; //En Milisegundos
-  static unsigned long previousMillis=0;
-  if((millis()-previousMillis)>period){//leer cada 7 segundos
-   if (ens160.available()) {
-    ens160.measure(true);
-    ens160.measureRaw(true);
-    Serial.print("AQI: ");Serial.print(ens160.getAQI());Serial.print("\t");
-    Serial.print("emerTVOC: ");Serial.print(ens160.getTVOC());Serial.print("ppb\t");
-    Serial.print("eCO2: ");Serial.print(ens160.geteCO2());Serial.print("ppm\t");
-    }
-    previousMillis += period;
-  }//millis
-}// leeens160
